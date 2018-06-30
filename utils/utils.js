fc6b32ecbc7998b0a969710b3d37e7acbe57e3e1
TerryDutton <t_dutton85@hotmail.com>
2018-06-30 16:05:56 +0100

    Created a function that takes a list of destinations and returns them sorted into groups for per-day scheduling

--- utils/utils.js
+++ utils/utils.js
@@ -1,15 +1,34 @@
 const axios = require('axios');
-const places = require('../demoSearchResults.json');
-const start = require('../demoStartPoint.json');
-const efficientRes = require('../demoSortedRoutes.json');
+const demoSearchResults = require('../demoSearchResults.json');
+const demoStartPoint = require('../demoStartPoint.json');
+const demoSortedRoutes = require('../demoSortedRoutes.json');
+const demoSortedDestinations = require('../demoSortedDestinations.json');
 const {app_id, app_code} = require('../config.js');
 const stripTitle = /[^a-zA-Z\d]/g;
 
+
+/*This function takes an array of HERE objects sorted by most efficient route, and the number of activities per day the user wants to do. It returns an array of arrays, with each nested array being a set of activities for a day. The function should spread activities to avoid any gross un-evenness - eg: 10 activities at 3 per day needs 4 days to do, but a schedule of 3,3,2,2 is better than 3,3,3,1.*/
+function sortIntoSets(activities, nPerDay){
+  let daysLeft = Math.ceil(activities.length / nPerDay); 
+  let scheduleArr = [];
+
+  activities = activities.slice();
+  while(activities.length && daysLeft){
+    const nForThisDay = Math.ceil(activities.length / daysLeft);
+    scheduleArr.push(
+      activities.splice(0,nForThisDay)
+    );
+    daysLeft--;
+  }
+
+  return scheduleArr;  
+}
+
 /*In each HERE place object, there is another object called 'openingHours'. This contains an array called 'structured', which contains further objects that break down what times and days the venue is open. This function takes that array and returns a simplified object of seven keys, one for each day. If the place is closed on that day, the value is NULL; otherwise, it's an array with two elements: the opening time, and the closing time. Not all HERE place objects have opening times, so that needs to be accounted for. */
-function getClarifiedOpeningTimes(arr){
+function getClarifiedOpeningTimes(activities){
   const convertNumToTimeString = n => '0'.repeat(4-`${n}`.length) + n;
 
-  return arr.reduce((acc, {start, duration, recurrence} ) => {
+  return activities.reduce((acc, {start, duration, recurrence} ) => {
     const openingTime = +start.slice(1,5); 
     const openForThisLong = (+duration.slice(2,4)*100) + (+duration.slice(-3,-1));
     const closingTime = (openingTime + openForThisLong) % 2400;
@@ -33,12 +52,12 @@ function sortDestinations(waypoints, destinations){
   return sortedDestinations;
 }
 
-/*startPoint is a HERE object for the hotel you stay at. arr is the array called 'items' that HERE gives on a returned search.*/
-function getQueryForEfficientDistance(startPoint, arr){ 
+/*startPoint is a HERE object for the hotel you stay at. activities is the array called 'items' that HERE gives on a returned search.*/
+function getQueryForEfficientDistance(startPoint, activities){ 
   const qStr = (title, position) => `${title.replace(stripTitle,'')};${position[0]},${position[1]}`;
 
   const startQuery = qStr(startPoint.title, startPoint.position); 
-  const visitQueries = arr.reduce((acc, {title, position}, i) => {
+  const visitQueries = activities.reduce((acc, {title, position}, i) => {
     return `${acc}&destination${i+1}=${qStr(title, position)}`;
   }, '');
   
