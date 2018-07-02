7a145c91772ce2193dd1c456c496b6c4811b0439
mdncs <madalinamaria.bumb@gmail.com>
2018-07-02 15:34:34 +0100

    post route fixed

--- controllers/itinerary.js
+++ controllers/itinerary.js
@@ -1,7 +1,6 @@
-const getSchedule = require('../utils/getSchedule');
+const { getSchedule } = require('../utils/getSchedule');
 
 exports.sendItinerary = (req, res, next) => {
-    console.log('hello')
     const { startPoint, activities, noPerDay } = req.body;
     return getSchedule(startPoint, activities, noPerDay)
         .then(schedule => res.send(schedule))
--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,4 @@
-const router = require('express')();
+const router = require('express').Router();
 const citiesRouter = require('./cities.js');
 const itineraryRouter = require('./itinerary.js');
 
