3bd1841acc7c2a8aaf0e673e9c9040ac99d78006
mdncs <madalinamaria.bumb@gmail.com>
2018-07-04 14:38:48 +0100

    added missing config

--- models/cities.js
+++ models/cities.js
@@ -18,7 +18,6 @@ const fetchPlacesByCity = cityName => {
         .catch(err => next(err));
 }
 
-
 // fetching directly from API by coordinates
 // const fetchPlacesByCity = cityName => {
 //     const coordinates = refObj()[cityName.toLowerCase()];
