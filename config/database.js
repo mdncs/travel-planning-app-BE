898fcfea4df645a96cdb78561ca794f21837c1bb
mdncs <madalinamaria.bumb@gmail.com>
2018-07-18 00:02:40 +0100

    added pdf presentation template file

--- config/database.js
+++
@@ -1,5 +0,0 @@
-const dbSession = {
-    neo4j: require('neo4j-driver'),
-    driver: neo4j.driver('bolt://hobby-kejliagjjjpngbkedcdfljbl.dbs.graphenedb.com:24786', neo4j.v1.auth.basic('production', 'b.6blrZy9Faq7A.icW7AUVODH1fqA3J')),
-    session: driver.session()
-}
